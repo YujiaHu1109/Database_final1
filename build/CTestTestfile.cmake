6107fea53343141b06de1b2443495bc12b4434d9
HuYujia <115564852+YujiaHu1109@users.noreply.github.com>
2022-11-19 17:14:48 +0800

    Add files via upload

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/cici1109/Desktop/DataBase-project/DataBase-project
+# Build directory: /home/cici1109/Desktop/DataBase-project/DataBase-project/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(src)
+subdirs(test)
